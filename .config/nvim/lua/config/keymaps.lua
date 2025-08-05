a92bc431309bb33ebe7e40192004cfb0e99af2ff
Mateo Roldos <mateoroldos19@gmail.com>
2025-08-05 12:39:33 -0300

    New stuff

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local snacks = require("snacks.picker")
+
+vim.keymap.set("n", "<leader>fh", snacks.git_status, { desc = "Git Changed Files (Snacks picker)" })
